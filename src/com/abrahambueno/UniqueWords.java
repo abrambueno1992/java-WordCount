a5ce6989b069ec21d123ddd92d3ce2541957f615
Abraham Bueno <abueno@localhost.localdomain>
2019-02-05 14:58:29 -0600

    Playing around

--- src/com/abrahambueno/UniqueWords.java
+++ src/com/abrahambueno/UniqueWords.java
@@ -10,8 +10,24 @@ public class UniqueWords {
 
     }
 
+    public void parseString(String str) {
+        private String temporary = "";
+        for (int i = 0; i <  str.length(); i++) {
+            if (str.charAt(i) != " " && str.charAt(i) != "." && str.charAt(i) != "," ) {
+                temporary += str.charAt(i);
+            } else {
+                if (temporary != "") {
+                    strArr.add(temporary);
+                }
+                temporary = "";
+            }
+        }
+        System.out.println("This is the new array, with words only: " + strArr);
+    }
+
     public UniqueWords(String longString) {
         this.longString = longString;
+        parseString(longString);
     }
 
     public String getLongString() {
