5077cc14480cabf963cba5a1ceac6c8bd36f57cb
Abraham Bueno <abueno@localhost.localdomain>
2019-02-05 19:53:15 -0600

    Have the 50 most common words, and cleaned up unnecessary code

--- src/com/abrahambueno/UniqueWords.java
+++ src/com/abrahambueno/UniqueWords.java
@@ -5,22 +5,13 @@ import java.util.Arrays;
 
 public class UniqueWords {
     private String longString;
-    private ArrayList<String> strArr = new ArrayList<String>();
-//    ArrayList<Dogs> dogsArrayList
     public static void main(String[] args) {
 
     }
 
 
 
-    public ArrayList<String> getStrArr() {
-        return strArr;
-    }
 
-    public void setStrArr(ArrayList<String> strArr, String longString) {
-        this.strArr = strArr;
-        this.longString = longString;
-    }
 
     public String getLongString() {
         return longString;
@@ -32,13 +23,13 @@ public class UniqueWords {
 
     public UniqueWords( String longString) {
         this.longString = longString;
-//        this.strArr = strArr;
+
     }
 
     @Override
     public String toString() {
         return "UniqueWords{" +
-                "strArr=" + strArr +
+                "longString='" + longString + '\'' +
                 '}';
     }
 }
